7018506482deb1c2e83be1b9a75896864caf8b83
Jelisa Butler <jelisa.l.butler@gmail.com>
2019-10-24 17:25:58 -0500

    made comments on what everything is doing

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
-//Dependcies:
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+// Tells node that we are creating an "express" server
 var app = express();
-var PORT = process.env.PORT || 3000;
 
-//Sets up the Express app to handle data parsing
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// BodyParser makes it possible for our server to interpret data sent to it.
+// The code below is pretty standard.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-//Router
+// ROUTES
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 require('./app/routing/api-routes.js')(app);
 require('./app/routing/html-routes.js')(app);
 
-//Starts the server to begin listening 
+// The below code effectively "starts" our server
 app.listen(PORT, function () {
     console.log('App listening on PORT: ' + PORT);
   });
